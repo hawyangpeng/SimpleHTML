e42b569f5c1a2aa84fb033abee46a94e68b5bbda
fzoyster <122716553+fzoyster@users.noreply.github.com>
2024-10-04 10:48:04 +1000

    async to SQL

--- SQL.js
+++ SQL.js
@@ -17,18 +17,42 @@ console.log('Connected to MySQL database!');
 }
 });
 
-
-connection.query("SELECT * FROM jptestdb.contacts", function (err, result, fields) {
-  if (err) throw err;
-  else {
-  testDB(result);
-  }
-});
-
-//Create a function that takes an argument. Run the function inside a block where you want to extract a variable.
-function testDB(a){
-    console.log(a)
+function getData (){
+  const gg = new Promise((resolve,reject)=>{
+    connection.query("SELECT * FROM jptestdb.contacts", function (err, result, fields) {
+      if (err) throw err;
+      else {
+      return resolve (result)
+      }
+    });
+  })
+  return gg
 }
 
+async function finalData(){
+  const data = await getData()
+  console.log(data)
+}
 
-module.exports = testDB()
+finalData()
+// function getData1 (){
+//   const promise1 = new Promise ((resolve,reject) => {
+//     resolve(connection.query("SELECT * FROM jptestdb.contacts", function (err, result, fields) {
+//       if (err) throw err;
+//       else {
+//       return result
+//       }}))})
+//   return promise1
+// }
+
+
+
+
+
+//Create a function that takes an argument. Run the function inside a block where you want to extract a variable.
+// function testDB(a){
+//     console.log(a)
+// }
+
+
+//module.exports = testDB()
